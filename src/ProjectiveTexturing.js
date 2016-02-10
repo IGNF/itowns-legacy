699cd87c0e6c3d73ad84f14aca6f321a9e4ba8f0
Mathieu Brédif <mathieu.bredif@ign.fr>
2016-02-10 19:03:14 +0100

    throttling down the number of panoramics (2->1) when needed to meet the hardware gl constraints.

--- src/ProjectiveTexturing.js
+++ src/ProjectiveTexturing.js
@@ -22,17 +22,42 @@
 
 				var _shaderMat = null;
 				var _initiated = false;
+				var _targetNbPanoramics;
 
 				var ProjectiveTexturing = {
-					init: function(){
+					
+					init: function(targetNbPanoramics){
+						_targetNbPanoramics = targetNbPanoramics || 2;
 						_initiated = true;
 					},
+					
 					isInitiated: function(){
 						return _initiated;
+					},	
+					
+					// display all the images of the panoramics
+					nbImages: function(){
+						return Ori.sensors.length;
 					},
-					createShaderMat: function(panoInfo,rot){
-						var P = 2; // panoramics
-						var N = Ori.sensors.length;
+					
+					// throttle down the number of panoramics to meet the gl.MAX_* constraints
+					nbPanoramics: function(){ 
+						var N = this.nbImages();
+						var gl = graphicEngine.getRenderer().getContext();
+						var maxVaryingVec = gl.getParameter(gl.MAX_VARYING_VECTORS);
+						var maxTextureImageUnits = gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS);
+						var maxNbPanoramics = Math.floor(Math.min(maxVaryingVec,Math.floor(maxTextureImageUnits/2))/N);
+						var P = Math.min(_targetNbPanoramics,maxNbPanoramics);
+						console.log("Images per panoramic  : ", N ,"/",N);
+						console.log("Panoramics displayed : ", P ,"/",_targetNbPanoramics);
+						console.log("Varying usage : ", (N*P) ,"/",maxVaryingVec);
+						console.log("Texture units usage : ", (2*N*P) ,"/",maxTextureImageUnits);
+						return P;
+					},
+					
+					createShaderMat: function(panoInfo,rot){  
+						var N = this.nbImages();
+						var P = this.nbPanoramics();
 						var uniforms = {}
 						var baseUrl = PanoramicProvider.getMetaDataSensorURL();
 						for (var i=0; i<N; ++i){
@@ -48,7 +73,7 @@
 								uniforms['pps['+j+']'] = {type:'v2',value:Ori.getPPS(i)};
 								uniforms['size['+j+']'] = {type:'v2',value:Ori.getSize(i)};
 								uniforms['mask['+j+']'] = {type:'t' ,value:Ori.getMask(i)};
-								uniforms['alpha['+j+']'] = {type:'f' ,value:pano};
+								uniforms['alpha['+j+']'] = {type:'f' ,value:1-pano};
 								uniforms['mvpp['+j+']'] = {type:'m3',value:mvpp};
 								uniforms['translation['+j+']'] = {type:'v3',value:trans};
 								uniforms['texture['+j+']'] = {type:'t' ,value:THREE.ImageUtils.loadTexture(tex)  };
@@ -66,10 +91,10 @@
             		},
 					tweenIndiceTime: function (i){
             			var alpha = _shaderMat.uniforms['alpha['+i+']'].value;
-            			if(alpha>0){
-	            			var j = i + Ori.sensors.length;
-                			alpha -= 0.03;
-                			if(alpha<0) alpha=0;
+            			if(alpha<1){
+	            			var j = i + this.nbImages();
+                			alpha += 0.03;
+                			if(alpha>1) alpha=1;
                 			_shaderMat.uniforms['alpha['+i+']'].value = alpha;
                 			_shaderMat.uniforms['alpha['+j+']'].value = 1-alpha;
                 			var that = this;
@@ -92,20 +117,22 @@
 							var mat = Ori.getMatrix(i).clone();
 							var mvpp = (new THREE.Matrix3().multiplyMatrices( rotation,mat )).transpose();
 	            			var trans = Ori.getSommet(i).clone().applyMatrix3(rotation);
-	            			var j = i + Ori.sensors.length;
-							_shaderMat.uniforms['mvpp['+i+']'].value = _shaderMat.uniforms['mvpp['+j+']'].value;
-							_shaderMat.uniforms['translation['+i+']'].value = _shaderMat.uniforms['translation['+j+']'].value;
-							_shaderMat.uniforms['texture['+i+']'].value =_shaderMat.uniforms['texture['+j+']'].value;
+	            			var j = i + that.nbImages();
+	            			if(_shaderMat.uniforms['mvpp['+j+']']) {
+								_shaderMat.uniforms['mvpp['+j+']'].value = _shaderMat.uniforms['mvpp['+i+']'].value;
+								_shaderMat.uniforms['translation['+j+']'].value = _shaderMat.uniforms['translation['+i+']'].value;
+								_shaderMat.uniforms['texture['+j+']'].value =_shaderMat.uniforms['texture['+i+']'].value;
+								_shaderMat.uniforms['alpha['+j+']'].value = 1;
+								_shaderMat.uniforms['alpha['+i+']' ].value = 0;
+								that.tweenIndiceTime(i);
+							}
 
-	            			_shaderMat.uniforms['mvpp['+j+']'].value = mvpp;
-	            			_shaderMat.uniforms['translation['+j+']'].value = translation.clone().add(trans);
-	            			_shaderMat.uniforms['texture['+j+']'].value = new THREE.Texture(this,THREE.UVMapping, 
+	            			_shaderMat.uniforms['mvpp['+i+']'].value = mvpp;
+	            			_shaderMat.uniforms['translation['+i+']'].value = translation.clone().add(trans);
+	            			_shaderMat.uniforms['texture['+i+']'].value = new THREE.Texture(this,THREE.UVMapping, 
 	            				THREE.RepeatWrapping, THREE.RepeatWrapping, THREE.LinearFilter,THREE.LinearFilter,THREE.RGBFormat);
-	            			_shaderMat.uniforms['texture['+j+']'].value.needsUpdate = true;
+	            			_shaderMat.uniforms['texture['+i+']'].value.needsUpdate = true;
 
-	            			_shaderMat.uniforms['alpha['+i+']' ].value = 1;			
-	            			_shaderMat.uniforms['alpha['+j+']'].value = 0;			
-            				that.tweenIndiceTime(i);
 						}; 
 						var baseUrl = PanoramicProvider.getMetaDataSensorURL();
 						var panoUrl = panoInfo.url_format.replace("{cam_id_pos}",Ori.sensors[i].infos.cam_id_pos);
