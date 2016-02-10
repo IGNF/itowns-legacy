8c16c0b25ebaa668c300f7da043cab95f1b1c90e
Thomas Broyer <t.broyer@ltgt.net>
2016-02-10 10:42:41 +0100

    Add script to deploy dist/ folder to gh-pages
    The script:
     * does not build the project, this must be done prior to calling it
     * only updates the dist/ folder in gh-pages as a copy of the dist/
       local folder
     * uses the local repository as "source", so you have to make sure
       your local gh-pages branch is "clean"
    
    To deploy a new version of iTowns, run the following commands:
    
        npm run build
        ./scripts/deploy_dist.sh

---
+++ scripts/deploy_dist.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Deploys the current iTowns dist to the gh-pages branch of the GitHub
+# repository.
+
+set -ex
+
+REPO_ROOT=$(dirname $(realpath $(dirname ${BASH_SOURCE[0]})))
+
+DIR=temp-itowns-clone
+
+# Delete any existing temporary gh-pages clone
+rm -rf $DIR
+
+# Clone the current repo into temp folder
+git clone -b gh-pages $REPO_ROOT $DIR
+
+# Move working directory into temp folder
+cd $DIR
+
+# Delete everything
+rm -rf ./dist/*
+
+# Copy website files from real repo
+cp -R ../dist/* ./dist/
+
+# Stage all files in git and create a commit
+git add ./dist/
+git add -u
+git commit -m "iTowns at $(date)"
+
+# Push the new files up to GitHub
+git push git@github.com:iTowns/itowns.git gh-pages
+
+# Delete our temp folder
+cd ..
+rm -rf $DIR
